9125a2fd3fd87efc556cfffd2d0a28ef0cfb0fc0
sachilles <kina98@gmx.ch>
2025-06-03 11:46:53 +0200

    Add employee account details component and enable stage modal
    This commit introduces a new Livewire component (`Details`) for handling employee profile account details, along with its associated Blade view. Additionally, it re-enables the "create-stage" modal trigger in the stage form Blade template.

---
+++ app/Livewire/Alem/Employee/Profile/Account/Details.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Alem\Employee\Profile\Account;
+
+use Livewire\Component;
+
+class Details extends Component
+{
+    public function render()
+    {
+        return view('livewire.alem.employee.profile.account.details');
+    }
+}
