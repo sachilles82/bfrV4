2e157a7045887bd5bf088ce5c0ca09ea2063d1e8
sachilles <kina98@gmx.ch>
2025-07-09 19:07:40 +0200

    Add dropdown actions (Edit/Delete) and additional child details to `Children` view in `Employee Profile`.

--- resources/views/livewire/alem/employee/profile/family/child.blade.php
+++ resources/views/livewire/alem/employee/profile/family/child.blade.php
@@ -35,6 +35,166 @@
                                 Alem Alija
                             </x-pupi.table2.tr.cell1>
                             <x-pupi.table2.tr.cell>
+                                männlich
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                11.01.2022
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                756.13.456.79
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                11.01.2038
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.action>
+                                <flux:dropdown align="end" offset="-15">
+                                    <flux:button class="hover:bg-gray-200/75" icon="ellipsis-horizontal"
+                                                 size="sm"
+                                                 variant="ghost" inset="top bottom"/>
+
+                                    <flux:menu class="min-w-32">
+                                        <flux:menu.item
+                                            {{--                                                                                            wire:click="showEditModal({{ $role->id }})"--}}
+                                            icon="pencil-square">
+                                            {{ __('Edit') }}
+                                        </flux:menu.item>
+
+                                        <flux:menu.item
+                                            {{--                                                                                            wire:click="delete({{ $role->id }})"--}}
+                                            wire:confirm="{{ __('Are you sure you want to remove this role?') }}"
+                                            wire:confirm.prompt="Are you sure?\n\nType YES to confirm|YES"
+                                            icon="trash" variant="danger">
+                                            {{ __('Delete') }}
+                                        </flux:menu.item>
+                                    </flux:menu>
+                                </flux:dropdown>
+                            </x-pupi.table2.tr.action>
+                        </x-pupi.table2.tr.body>
+                        <x-pupi.table2.tr.body>
+                            <x-pupi.table2.tr.cell1>
+                                Alem Alija
+                            </x-pupi.table2.tr.cell1>
+                            <x-pupi.table2.tr.cell>
+                                Männlich
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                11.01.2022
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                756.13.456.79
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                11.01.2038
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.action>
+                                <flux:dropdown align="end" offset="-15">
+                                    <flux:button class="hover:bg-gray-200/75" icon="ellipsis-horizontal"
+                                                 size="sm"
+                                                 variant="ghost" inset="top bottom"/>
+
+                                    <flux:menu class="min-w-32">
+                                        <flux:menu.item
+                                            {{--                                                                                            wire:click="showEditModal({{ $role->id }})"--}}
+                                            icon="pencil-square">
+                                            {{ __('Edit') }}
+                                        </flux:menu.item>
+
+                                        <flux:menu.item
+                                            {{--                                                                                            wire:click="delete({{ $role->id }})"--}}
+                                            wire:confirm="{{ __('Are you sure you want to remove this role?') }}"
+                                            wire:confirm.prompt="Are you sure?\n\nType YES to confirm|YES"
+                                            icon="trash" variant="danger">
+                                            {{ __('Delete') }}
+                                        </flux:menu.item>
+                                    </flux:menu>
+                                </flux:dropdown>
+                            </x-pupi.table2.tr.action>
+                        </x-pupi.table2.tr.body>
+                        <x-pupi.table2.tr.body>
+                            <x-pupi.table2.tr.cell1>
+                                Alem Alija
+                            </x-pupi.table2.tr.cell1>
+                            <x-pupi.table2.tr.cell>
+                                Männlich
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                11.01.2022
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                756.13.456.79
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                11.01.2038
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.action>
+                                <flux:dropdown align="end" offset="-15">
+                                    <flux:button class="hover:bg-gray-200/75" icon="ellipsis-horizontal"
+                                                 size="sm"
+                                                 variant="ghost" inset="top bottom"/>
+
+                                    <flux:menu class="min-w-32">
+                                        <flux:menu.item
+                                            {{--                                                                                            wire:click="showEditModal({{ $role->id }})"--}}
+                                            icon="pencil-square">
+                                            {{ __('Edit') }}
+                                        </flux:menu.item>
+
+                                        <flux:menu.item
+                                            {{--                                                                                            wire:click="delete({{ $role->id }})"--}}
+                                            wire:confirm="{{ __('Are you sure you want to remove this role?') }}"
+                                            wire:confirm.prompt="Are you sure?\n\nType YES to confirm|YES"
+                                            icon="trash" variant="danger">
+                                            {{ __('Delete') }}
+                                        </flux:menu.item>
+                                    </flux:menu>
+                                </flux:dropdown>
+                            </x-pupi.table2.tr.action>
+                        </x-pupi.table2.tr.body>
+                        <x-pupi.table2.tr.body>
+                            <x-pupi.table2.tr.cell1>
+                                Alem Alija
+                            </x-pupi.table2.tr.cell1>
+                            <x-pupi.table2.tr.cell>
+                                Männlich
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                11.01.2022
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                756.13.456.79
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.cell>
+                                11.01.2038
+                            </x-pupi.table2.tr.cell>
+                            <x-pupi.table2.tr.action>
+                                <flux:dropdown align="end" offset="-15">
+                                    <flux:button class="hover:bg-gray-200/75" icon="ellipsis-horizontal"
+                                                 size="sm"
+                                                 variant="ghost" inset="top bottom"/>
+
+                                    <flux:menu class="min-w-32">
+                                        <flux:menu.item
+                                            {{--                                                                                            wire:click="showEditModal({{ $role->id }})"--}}
+                                            icon="pencil-square">
+                                            {{ __('Edit') }}
+                                        </flux:menu.item>
+
+                                        <flux:menu.item
+                                            {{--                                                                                            wire:click="delete({{ $role->id }})"--}}
+                                            wire:confirm="{{ __('Are you sure you want to remove this role?') }}"
+                                            wire:confirm.prompt="Are you sure?\n\nType YES to confirm|YES"
+                                            icon="trash" variant="danger">
+                                            {{ __('Delete') }}
+                                        </flux:menu.item>
+                                    </flux:menu>
+                                </flux:dropdown>
+                            </x-pupi.table2.tr.action>
+                        </x-pupi.table2.tr.body>
+                        <x-pupi.table2.tr.body>
+                            <x-pupi.table2.tr.cell1>
+                                Alem Alija
+                            </x-pupi.table2.tr.cell1>
+                            <x-pupi.table2.tr.cell>
                                 Männlich
                             </x-pupi.table2.tr.cell>
                             <x-pupi.table2.tr.cell>
